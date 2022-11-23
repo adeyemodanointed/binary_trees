9bc51d4c877fd484e8d9e3ef1b7722810df32cbd
adeyemodanointed <adeyemodanointed@yahoo.com>
2022-11-23 23:57:00 +0000

    task 4

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	binary_tree_t *newNode;
 
-	newNode = malloc(sizeof(binary_tree_t));
+	newNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 
 	if (!newNode)
 		return (NULL);
@@ -30,6 +30,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	else
 	{
+		newNode->parent = parent;
 		if (parent->left == NULL)
 		{
 			parent->left = newNode;
@@ -39,9 +40,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 			newNode->left = parent->left;
 			newNode->left->parent = newNode;
 			parent->left = newNode;
-		}
-		newNode->parent = parent;
+		}	
 	}
-	return (newNode);
+
+	return (parent->left);
 }
 
